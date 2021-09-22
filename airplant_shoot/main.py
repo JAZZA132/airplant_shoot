7dcd446e562acc1dd7320378e70c781026dece66
JAZZA132 <66157474+JAZZA132@users.noreply.github.com>
2021-09-22 17:16:11 +0800

    Add files via upload

---
+++ airplant_shoot/main.py
@@ -0,0 +1,193 @@
+
+import pygame
+import random
+import os
+from pygame import image
+from pygame import key
+from pygame import transform
+from pygame.constants import K_SPACE, KEYDOWN
+
+FPS = 60
+#(R,G,B)
+
+WIDTH = 500
+HIGHT = 600
+
+
+WHITE = (255,255,255)
+GREEN = (0,255,0)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+BLACK = (0,0,0)
+
+# 遊戲初始畫 and 創建視窗
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH,HIGHT))
+pygame.display.set_caption("飛機射擊")
+clock = pygame.time.Clock()
+
+# 載入圖片
+background_img = pygame.image.load(os.path.join("img","background.png")).convert()  # 轉換成pygame容易讀取的格式
+player_img = pygame.image.load(os.path.join("img","player.png")).convert()  # 轉換成pygame容易讀取的格式
+# rock_img = pygame.image.load(os.path.join("img","rock.png")).convert()  # 轉換成pygame容易讀取的格式
+bullet_img = pygame.image.load(os.path.join("img","bullet.png")).convert()  # 轉換成pygame容易讀取的格式
+rock_imgs = []
+for i in range(7):
+    rock_imgs.append(pygame.image.load(os.path.join("img",f"rock{i}.png")).convert())
+
+#載入音樂
+shoot_sound = pygame.mixer.Sound(os.path.join("sound","shoot.wav"))
+expl_sound = [
+    pygame.mixer.Sound(os.path.join("sound","expl0.wav")),
+    pygame.mixer.Sound(os.path.join("sound","expl1.wav"))
+]
+pygame.mixer.music.load(os.path.join("sound","background.ogg"))
+pygame.mixer.music.set_volume(0.4)
+
+
+font_name = pygame.font.match_font('arial')
+def draw_text(surf,text,size,x,y):
+    font = pygame.font.Font(font_name,size)
+    text_surface = font.render(text,True,WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.centerx = x
+    text_rect.top = y
+    surf.blit(text_surface,text_rect)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale( player_img,(50,38))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.radius = 20
+        # pygame.draw.circle(self.image,RED,self.rect.center,self.radius)
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HIGHT - 10
+        self.speedx = 8
+        # self.rect.x = 400
+        # self.rect.y = 400
+    def update(self):
+        key_pressed = pygame.key.get_pressed()
+        if key_pressed[pygame.K_RIGHT]:
+            self.rect.x += self.speedx
+        if key_pressed[pygame.K_LEFT]:
+            self.rect.x -= self.speedx   
+        
+        if self.rect.right >WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+    
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx,self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+        shoot_sound.play()
+
+class Rock(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image_ori = random.choice(rock_imgs) 
+        self.image_ori.set_colorkey(BLACK)
+        self.image = self.image_ori.copy()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width*0.85 / 2)
+        # pygame.draw.circle(self.image,RED,self.rect.center,self.radius)
+        self.rect.x = random.randrange(0,WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-180,-100)
+        self.speedy = random.randrange(2,10)
+        self.speedx = random.randrange(-3,3)
+        self.total_degree = 0
+        self.rot_degree = random.randrange(-3,3)
+
+    def rotate(self):
+        self.total_degree += self.rot_degree
+        self.total_degree = self.total_degree % 360
+        self.image = pygame.transform.rotate(self.image_ori ,self.total_degree)  #疊加太多次會失真
+        center = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+
+
+    def update(self):
+        self.rotate()
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        if self.rect.top > HIGHT or self.rect.left > WIDTH or self.rect.right < 0 :
+            self.rect.x = random.randrange(0,WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100,-40)
+            self.speedy = random.randrange(2,10)
+            self.speedx = random.randrange(-3,3)
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,x ,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speedy = -10
+        
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0 :
+            self.kill()
+
+
+all_sprites = pygame.sprite.Group()
+rocks = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+for i in range(8):
+    r = Rock()
+    all_sprites.add(r)
+    rocks.add(r)
+score = 0
+pygame.mixer.music.play(-1)
+
+
+# 遊戲迴圈
+running = True
+while running:
+    #一秒鐘之內最多執行幾次
+    clock.tick(FPS)
+    
+    # 取得輸入(所有動作)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+
+    # 更新遊戲
+    all_sprites.update()
+    hits = pygame.sprite.groupcollide(rocks,bullets,True,True)
+    for hit in hits:
+        random.choice(expl_sound).play()
+        score += hit.radius
+        r = Rock()
+        all_sprites.add(r)
+        rocks.add(r)
+
+    hits = pygame.sprite.spritecollide(player,rocks,False, pygame.sprite.collide_circle)
+    if hits:
+        running = False
+    
+    # 畫面顯示
+    screen.fill(BLACK)  
+    # 畫到畫面上,左上(0,0)開始渲染
+    screen.blit(background_img,(0,0))
+    all_sprites.draw(screen)
+    draw_text(screen, str(score),18,WIDTH/2,10)
+    pygame.display.update()
+
+
+pygame.quit()
